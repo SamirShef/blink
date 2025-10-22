5261f390095ba3b29d45191585117178749a119c
SamirShef <ssserin10@gmail.com>
2025-10-22 20:32:45 +0000

    Add token's struct

---
+++ include/lexer/token.hpp
@@ -0,0 +1,28 @@
+#include <string>
+
+enum class TokenType { SIGNED, UNSIGNED, 
+  CHAR,
+  SHORT,
+  INT,
+  LONG,
+  FLOAT,
+  DOUBLE,
+  VOID,
+ 
+  TYPEDEF,
+  STRUCT,
+  
+  SIZEOF,
+  IF,
+  ELSE,
+  WHILE, 
+  DO,
+  FOR,
+  BREAK,
+  CONTINUE,
+};
+
+struct Token {
+  TokenType type;
+  std::string value;
+};
